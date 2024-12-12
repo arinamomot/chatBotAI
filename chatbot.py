55957cf0dcbf6981c25c284326f9d7f055209c4c
Arina Momot <arinamomot@gmail.com>
2024-12-12 16:28:16 +0100

    Chatbot improvements

--- chatbot.py
+++ chatbot.py
@@ -10,26 +10,28 @@ from langchain.chains.question_answering import load_qa_chain
 
 # Load environment variables from .env file
 load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# Access the API key
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    st.error("Please set your OpenAI API key in the .env file.")
+    st.stop()
 
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+# Streamlit UI
 st.header("Chatbot")
 
 # Upload a PDF file
 with st.sidebar:
     st.title("Your Documents")
-    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+    file = st.file_uploader("Upload a PDF file here to start asking questions", type="pdf")
 
-# Extract text from PDF
-if file is not None:
+if file:
+    # Extract text from PDF
     reader = PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-        #st.write(text)
+    text = "".join(page.extract_text() for page in reader.pages)
 
-# Break it into chunks
+    # Split text into manageable chunks
     text_splitter = RecursiveCharacterTextSplitter(
         separators="\n",
         chunk_size=1000,
@@ -37,32 +39,33 @@ if file is not None:
         length_function=len
     )
     chunks = text_splitter.split_text(text)
-    # st.write(chunks)
 
-# Generating embeddings
-    # embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    # Generate embeddings
     embeddings = OpenAIEmbeddings()
 
-# Creating vector storev - FAISS (Facebook AI Similarity Search)
+    # Create vector store - FAISS (Facebook AI Similarity Search)
     vector_store = FAISS.from_texts(chunks, embeddings)
 
-# Get User question
-    user_question = st.text_input("Type your question here")
+    # Get User input
+    user_question = st.text_input("Enter your question below:")
 
-# Do similarity search
     if user_question:
+        # Perform similarity search
         match = vector_store.similarity_search(user_question)
-        # st.write(match)
 
-        # Define the LLM
+        # Set up the LLM
         llm = ChatOpenAI(
-            openai_api_key=os.getenv("OPENAI_API_KEY"),
+            openai_api_key=OPENAI_API_KEY,
             temperature=0,
             max_tokens=1000,
             model_name="gpt-3.5-turbo"
         )
 
-# Output results
+        # Process the query and provide a response
         chain = load_qa_chain(llm, chain_type="stuff")
         response = chain.run(input_documents=match, question=user_question)
+
+        # Display the response
         st.write(response)
+else:
+    st.info("Upload a PDF file to begin exploring its content interactively.")
